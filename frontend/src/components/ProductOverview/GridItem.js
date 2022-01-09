85c6aa94c6b71d0a9b42690b8c206e1c36ff4d50
ishkhasat <ishkhasat@outlook.com>
2022-01-09 11:11:13 +0400

    Adding-Buttons

--- frontend/src/components/ProductOverview/GridItem.js
+++ frontend/src/components/ProductOverview/GridItem.js
@@ -1,13 +1,27 @@
-﻿import React from "react";
+﻿import React, { useState } from "react";
 import ImgGreyBox from "../../images/GreyBox.svg"
 import PropTypes from "prop-types";
 
+
+const buy = <button type="button" className="btn btn-primary">Buy</button>
+
+
+
+
 const GridItem = ({ item }) => {
+  const [count, setCount] = useState(0)
   return (
     <div className="col-md-4 col-sm-12 p-5">
       <img src={ImgGreyBox} alt="Default Grey Box" className="mb-3" />
       <h3>{item.header}</h3>
-      <p>{item.shortDescription}</p>
+      <p>{item.title}</p>
+      <p>{item.Price}</p>
+      <p>{count}</p>
+      <div className="btn-group">
+      <p><button className="btn btn-success mx-3" onClick={() => setCount(count + 1)} disabled={count === 20}>+</button> </p>
+      <p><button className="btn btn-danger mx-3" onClick={() => setCount(count - 1)} disabled={count === 0}>-</button></p>
+      </div>
+      <p>{buy}</p>
     </div>
   );
 }
